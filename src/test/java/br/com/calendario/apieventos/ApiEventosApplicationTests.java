c8d9bcf3d7d79505fd5486315e067c82266c04bf
luanssouza <luan.souza27@hotmail.com>
2020-04-11 11:59:36 -0300

    init

---
+++ src/test/java/br/com/calendario/apieventos/ApiEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.calendario.apieventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
